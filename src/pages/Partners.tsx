f8e9329251a6b435404875d3c0b8226c75b39141
0uma0tieno <oumaruud@gmail.com>
2025-07-04 16:52:22 +0300

    Update Partners.tsx
    Trying to increase the height of the partner logos

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -1,17 +1,34 @@
 import { partnersData } from "../constants";
 
-{partnersData.map((partner) => (
-  <a
-    key={partner.name}
-    href={partner.linkUrl}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="inline-block"
-  >
-    <img
-      src={partner.logoUrl}
-      alt={partner.name}
-      className="h-50 w-auto" // Increase h-20 for a larger logo (Tailwind), or use style={{height: '80px'}}
-    />
-  </a>
-))}
+const Partners = () => (
+  <div className="flex flex-wrap items-center justify-center gap-8">
+    {partnersData.map((partner) =>
+      partner.linkUrl ? (
+        <a
+          key={partner.name}
+          href={partner.linkUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block"
+        >
+          <img
+            src={partner.logoUrl}
+            alt={partner.name}
+            className="h-20 w-auto object-contain transition-transform hover:scale-105"
+            style={{ maxHeight: 100 }}
+          />
+        </a>
+      ) : (
+        <img
+          key={partner.name}
+          src={partner.logoUrl}
+          alt={partner.name}
+          className="h-20 w-auto object-contain"
+          style={{ maxHeight: 100 }}
+        />
+      )
+    )}
+  </div>
+);
+
+export default Partners;
