75d98bf0b4c51d2abafe6ba1b309e969e6cf7d14
0uma0tieno <oumaruud@gmail.com>
2025-07-04 15:42:27 +0300

    updates
    a couple of things

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,9 +32,11 @@ const Header: React.FC = () => {
         <>
             <header className={`sticky top-0 left-0 w-full z-50 transition-all duration-300 ${scrolled || isMenuOpen ? 'bg-white/95 shadow-md backdrop-blur-sm' : 'bg-transparent'}`}>
                 <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-                    <Link to="/" className="text-2xl font-bold text-[#293855] transition-transform hover:scale-105">
-                        The Lads<span className="text-[#F1AC20]">.</span>
-                    </Link>
+                    <img 
+                        src="public/images/the lads favicon.png"
+                        alt="The lads logo"
+                        className="mr-2 h-16 w-auto"
+                    />
                     <nav className="hidden md:flex items-center space-x-8">
                         {navLinks.map((link) => (
                             <Link 
