6e2c03467ec11adbb3aa76e558657f25f95eaf66
0uma0tieno <oumaruud@gmail.com>
2025-07-06 04:27:54 +0300

    Navigation
    Updtaed the anaviagation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import Hero from '../sections/Hero';
-import Projects from './Projects';
-import Events from './Events';
+import Projects from '../sections/Projects';
+import Events from '../sections/Events';
 import Partners from '../sections/Partners';
 import Team from '../sections/Team';
-import Blog from './Blog';
-import Sponsors from './SponsorPage';
-import Contact from './Contact';
+import Blog from '../sections/Blog';
+import Sponsors from '../sections/Sponsors';
+import Contact from '../sections/Contact';
 
 const HomePage: React.FC = () => {
     return (
